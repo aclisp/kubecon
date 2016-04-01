29d2d9dea0d50bc32f25ebeb36cc24a8558ca538
Homer Huang <aclisp@gmail.com>
2016-04-01 21:59:11 +0800

    jslint

--- js/page.js
+++ js/page.js
@@ -23,10 +23,11 @@ function post(path, params, method) {
 }
 
 function serialize(obj) {
-  var str = [];
-  for(var p in obj)
-    if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    var str = [];
+    for(var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
     }
-  return str.join("&");
+    return str.join("&");
 }
