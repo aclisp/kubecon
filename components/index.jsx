59533b660a06d8ccb8d6705a47900cf3b8493f65
Homer Huang <aclisp@gmail.com>
2016-06-03 15:29:03 +0800

    Revert "Add first app form."
    This reverts commit 45e81541cf643376b5b76f17e125f97db12e896a.

--- components/index.jsx
+++ components/index.jsx
@@ -1,83 +1,12 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
-import ApplicationItem from './ApplicationItem';
-import UnimplementedApplication from './UnimplementedApplication';
-import HDFSApp from './HDFSApp';
 
-let appList = [
-  {
-    id: 'zookeeper',
-    title: 'ZooKeeper',
-  },
-  {
-    id: 'mongodb',
-    title: 'MongoDB',
-  },
-  {
-    id: 'mysql',
-    title: 'MySQL',
-  },
-  {
-    id: 'postgres',
-    title: 'PostgreSQL',
-  },
-  {
-    id: 'hdfs',
-    title: 'HDFS',
-  },
-  {
-    id: 'yarn',
-    title: 'Hadoop YARN',
-  },
-  {
-    id: 'mapreduce',
-    title: 'Hadoop MapReduce',
-  },
-  {
-    id: 'spark',
-    title: 'Spark',
-  },
-  {
-    id: 'storm',
-    title: 'Storm',
-  },
-  {
-    id: 'tensorflow',
-    title: 'TensorFlow',
-  },
-];
-
-class ApplicationGallery extends Component {
+class Hello extends Component {
   render() {
-    let apps = this.props.route.apps.map((app) =>
-      <ApplicationItem app={app} key={app.id} />
-    );
-
     return (
-      <div>
-        <h1 className="page-header">Application Gallery</h1>
-        <div className="container-fluid">
-          <div className="row">
-            {apps}
-          </div>
-        </div>
-        {this.props.children}
-      </div>
+      <h1>Hello Wor</h1>
     );
   }
 }
 
-ApplicationGallery.propTypes = {
-  children: PropTypes.node,
-  route: PropTypes.object,
-};
-
-render((
-  <Router history={hashHistory}>
-    <Route path="/" component={ApplicationGallery} apps={appList}>
-      <Route path="new/hdfs" component={HDFSApp} />
-      <Route path="new/:app_id" component={UnimplementedApplication} apps={appList} />
-    </Route>
-  </Router>
-), document.getElementById('root'));
+render(<Hello />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
   output: {
-    path: path.join(__dirname, '.'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
   },
   resolve: {
